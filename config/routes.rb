cc791ca95ac56a8f141821d3130e2bcf407caaf5
nobuko.m <animopara100pre@gmail.com>
2020-08-11 07:15:59 +0000

    to_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'root/top'
 #rootメソッド
 root :to => 'root#top'
-resources :books
+resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
