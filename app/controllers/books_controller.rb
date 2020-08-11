ddfcc79c907c659a0119b65282076edd334f3e11
nobuko.m <animopara100pre@gmail.com>
2020-08-11 01:45:33 +0000

    To_Baritation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,44 @@ class BooksController < ApplicationController
   
   
   def index
+    @books = Book.all
+  end
+  
+  def new
+    @books =Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
+    puts "Book was successfully created"
+  end
+  
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+    puts "Book was successfully updated"
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    puts "Book was successfully deleted"
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'root/top'
 #rootメソッド
 root :to => 'root#top'
-resources :blogs
+resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
